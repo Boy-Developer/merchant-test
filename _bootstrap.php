6c10607d4e058fcaefb79dfa62b99768632a4ed3
Boy-Developer <36400152+Boy-Developer@users.noreply.github.com>
2020-05-06 15:29:23 +0700

    Update _bootstrap.php

--- _bootstrap.php
+++ _bootstrap.php
@@ -33,7 +33,7 @@ $apiVersion = getenv('GATEWAY_API_VERSION');
 
 // merchant id must be TEST
 $merchantIdPrefix = substr($merchantId, 0, 4);
-if (strcasecmp($merchantIdPrefix, "1230") != 0) {
+if (strcasecmp($merchantIdPrefix, "9003") != 0) {
     error(500, 'Only TEST merchant IDs should be used with this software');
 }
 
